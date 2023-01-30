85189e5e35a45a56a3c06123785b9f4a2510a643
Andrew <drewsauer0623@gmail.com>
2023-01-30 12:43:58 -0500

    Begin programmming video stream

--- camalert.py
+++ camalert.py
@@ -1,1 +1,12 @@
-print("heheh")
+import cv2 
+import time
+stream = cv2.VideoCapture(0) 
+def motion_checker():
+    is_motion = False
+    while(is_motion==False):
+        isFrame, frame = stream.read()
+        cv2.imshow('cum', frame)
+        cv2.waitKey(1)
+
+
+motion_checker()
