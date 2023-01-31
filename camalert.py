a646f94a3cb776b6b4b060a07c71bcfc0862ee74
Andrew <drewsauer0623@gmail.com>
2023-01-31 18:03:56 -0500

    Add adaptive thresholding to motion detection code

--- camalert.py
+++ camalert.py
@@ -12,7 +12,7 @@ def motion_checker():
         frame_difference = cv2.absdiff(frame, comp_frame)
         frame_gray = cv2.cvtColor(frame_difference, cv2.COLOR_BGR2GRAY)
         frame_blur = cv2.GaussianBlur(frame_gray, (5,5), 0)
-        trash, frame_thresh = cv2.threshold(frame_blur, 40, 255, cv2.THRESH_BINARY)
+        frame_thresh = cv2.adaptiveThreshold(frame_blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 27, 15)
         frame_dilated = cv2.dilate(frame_thresh, None, iterations=3)
         contours_list, trash = cv2.findContours(frame_dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         biggest_contour_area = 0
@@ -20,7 +20,6 @@ def motion_checker():
             if cv2.contourArea(contour) > biggest_contour_area:
                 biggest_contour_area = cv2.contourArea(contour)
                 big_contour = contour
-        print(biggest_contour_area)
         (x, y, w, h) = cv2.boundingRect(big_contour)
         mean_x = int(statistics.mean([x, (x+w)]))
         mean_y = int(statistics.mean([y, (y+h)]))
